67f38d4c6c7ec39f18ded56b3d8074dcd31a5921
Dynesshely <2369913161@qq.com>
2022-02-22 20:02:14 +0800

    💧 又水一题

---
+++ luogu/P5082.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n, tmp;
+long long sum_per_sub_max = 0, sum_per_sub_real = 0;
+int main(){
+    scanf("%d", &n);
+    for(int i = 0; i < n; ++ i){
+        scanf("%d", &tmp);
+        sum_per_sub_max += tmp;
+    }
+    for(int i = 0; i < n; ++ i){
+        scanf("%d", &tmp);
+        sum_per_sub_real += tmp;
+    }
+    printf("%0.6f\n", ((double)sum_per_sub_max * 3 - (double)sum_per_sub_real * 2) / (double)(sum_per_sub_max - sum_per_sub_real));
+    return 0;
+}
