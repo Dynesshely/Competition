41e589323f6d6b3e3f50592418ef20b7194f5874
Dynesshely <2369913161@qq.com>
2022-03-08 21:10:47 +0800

    🆎 P1572 from luogu

---
+++ oj/luogu/P1572.cpp
@@ -0,0 +1,17 @@
+#include<cstdio>
+int a, b, c, d;
+int gcd(int x,int y){
+    if(y == 0) return x;
+    return gcd(y, x % y);
+}
+int main(){
+    scanf("%d/%d", &a, &b);
+    while(scanf("%d/%d", &c, &d) != EOF){
+        int m = gcd(b, d); a *= d / m; c *= b / m; a += c;
+        b *= d / m; m = gcd(a, b); a /= m; b /= m;
+    }
+    if(b < 0){ a = -a; b = -b; }
+    if(b == 1) printf("%d\n", a);
+    else printf("%d/%d\n", a, b);
+    return 0;
+}
