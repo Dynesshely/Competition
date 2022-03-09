cb44aee27f69e9ed6b6687ebabb5a0df8d7d46df
Dynesshely <2369913161@qq.com>
2022-03-09 08:26:41 +0800

    🎶 天空之城

---
+++ N13/Beep/天空之城.cpp
@@ -0,0 +1,411 @@
+#include <bits/stdc++.h>
+#include <windows.h>
+
+#define qdo	262
+#define qre	294
+#define qmi	330
+#define qfa	349
+#define qso	392
+#define qla	440
+#define qsi	494
+#define do	523
+#define re	578
+#define mi	659
+#define fa	698
+#define so	784
+#define la	880
+#define si	988
+#define do1	1046
+#define re1	1175
+#define mi1	1318
+#define fa1	1480
+#define so1	1568
+#define la1	1760
+#define si1	1976
+#define sqdo	277
+#define sqre	311
+#define sqfa	370
+#define sqso	415
+#define sqla	466
+#define sdo	554
+#define sre	622
+#define sfa	740
+#define sso	831
+#define sla	932
+#define sdo1	1046
+#define sre1	1245
+#define sfa1	1480
+#define sso1	1661
+#define sla1	1865
+
+using namespace std;
+
+int main(){
+    int	pai	= 400, ban = 200;
+    int	ting	= 128;
+
+    Sleep( 1000 );
+
+    Beep( la, ban );
+    Beep( si, ban );
+    Sleep( ting );
+
+    Beep( do1, pai + ban );
+    Beep( si, ban );
+    Sleep( ting );
+    Beep( do1, pai );
+    Sleep( ting );
+    Beep( mi1, pai );
+    Sleep( ting );
+
+    Beep( si, 3 * pai );
+    Sleep( ting );
+    Beep( mi, ban );
+    Beep( mi, ban );
+
+    Beep( la, ban + pai );
+    Beep( so, ban );
+    Sleep( ting );
+    Beep( la, pai );
+    Sleep( ting );
+    Beep( do1, pai );
+    Sleep( ting );
+
+    Beep( so, 2 * pai );
+    Sleep( ting );
+    Sleep( pai );
+    Beep( mi, ban );
+    Sleep( ting / 2 );
+    Beep( mi, ban );
+    Sleep( ting / 2 );
+
+    Beep( fa, pai + ban );
+    Beep( mi, ban );
+    Sleep( ting );
+    Beep( fa, ban );
+    Beep( do1, ban + pai );
+    Sleep( ting );
+
+    Beep( mi, 2 * pai );
+    Sleep( ting );
+    Sleep( ban );
+    Beep( do1, ban );
+    Sleep( ting / 2 );
+    Beep( do1, ban );
+    Sleep( ting / 2 );
+    Beep( do1, ban );
+    Sleep( ting / 2 );
+
+    Beep( si, ban + pai );
+    Beep( sfa, ban );
+    Sleep( ting );
+    Beep( sfa, pai );
+    Beep( si, pai );
+    Sleep( ting );
+
+    Beep( si, 2 * pai );
+    Sleep( ting );
+    Sleep( pai );
+    Beep( la, ban );
+    Beep( si, ban );
+    Sleep( ting );
+
+    Beep( do1, pai + ban );
+    Beep( si, ban );
+    Sleep( ting );
+    Beep( do1, pai );
+    Sleep( ting );
+    Beep( mi1, pai );
+    Sleep( ting );
+
+    Beep( si, 2 * pai );
+    Sleep( ting );
+    Sleep( pai );
+    Beep( mi, ban );
+    Sleep( 20 );
+    Beep( mi, ban );
+    Sleep( ting );
+
+    Beep( la, pai + ban );
+    Beep( so, ban );
+    Sleep( ting );
+    Beep( la, pai );
+    Sleep( ting );
+    Beep( do1, pai );
+    Sleep( ting );
+
+    Beep( so, 3 * pai );
+    Sleep( ting + ban );
+    Beep( mi, ban );
+    Sleep( ting / 2 );
+
+    Beep( fa, pai );
+    Sleep( ting );
+    Beep( do1, ban );
+    Beep( si, ban );
+    Sleep( 20 );
+    Beep( si, pai );
+    Sleep( ting );
+    Beep( do1, pai );
+    Sleep( ting );
+
+    Beep( re1, ban );
+    Sleep( 20 );
+    Beep( re1, ban );
+    Sleep( 20 );
+    Beep( mi1, ban );
+    Sleep( ting / 2 );
+    Beep( do1, pai );
+    Sleep( ting + pai );
+
+    Beep( do1, pai );
+    Beep( si, ban );
+    Sleep( ting );
+    Beep( la, ban );
+    Sleep( 20 );
+    Beep( la, ban );
+    Sleep( ting );
+    Beep( si, pai );
+    Sleep( ting );
+    Beep( sso, pai );
+    Sleep( ting );
+
+    Beep( sso, 2 * pai );
+    Sleep( ting + pai );
+    Beep( do1, ban );
+    Beep( re1, ban );
+    Sleep( ting );
+
+    Beep( mi1, pai + ban );
+    Beep( re1, ban );
+    Sleep( ting );
+    Beep( mi1, pai );
+    Sleep( ting );
+    Beep( fa1, pai );
+    Sleep( ting );
+
+    Beep( re1, 2 * pai );
+    Sleep( pai + ting );
+    Beep( so, ban );
+    Sleep( 20 );
+    Beep( so, ban );
+    Sleep( ting );
+
+    Beep( do1, ban );
+    Beep( si, ban );
+    Sleep( ting );
+    Beep( do1, pai );
+    Sleep( ting );
+    Beep( mi1, pai );
+    Sleep( ting );
+
+    Beep( mi1, 2 * pai );
+    Sleep( ting + 2 * pai );
+
+    Beep( la, ban );
+    Beep( si, ban );
+    Sleep( ting );
+    Beep( do1, pai );
+    Sleep( ting );
+    Beep( si, pai );
+    Sleep( ting );
+    Beep( re1, ban );
+    Sleep( 20 );
+    Beep( re1, ban );
+    Sleep( ting );
+
+    Beep( do1, pai + ban );
+    Beep( so, ban );
+    Sleep( 20 );
+    Beep( so, pai );
+    Sleep( pai + ting );
+
+    Beep( fa1, pai );
+    Sleep( ting );
+    Beep( mi1, pai );
+    Sleep( ting );
+    Beep( re1, pai );
+    Sleep( ting );
+    Beep( do1, pai );
+    Sleep( ting );
+
+    Beep( mi1, 4 * pai );
+
+    Beep( mi1, pai * 2 );
+    Sleep( pai + ting );
+    Beep( mi1, pai );
+    Sleep( ting );
+
+    Beep( la1, 2 * pai );
+    Sleep( ting );
+    Beep( so1, pai );
+    Sleep( ting );
+    Beep( so1, pai );
+    Sleep( ting );
+
+    Beep( mi1, ban );
+    Sleep( ting / 2 );
+    Beep( re1, ban );
+    Sleep( ting );
+    Beep( do1, pai );
+    Sleep( ting + ban );
+    Beep( do1, ban );
+    Sleep( ting );
+
+    Beep( re1, pai );
+    Sleep( ting );
+    Beep( do1, ban );
+    Beep( re1, ban );
+    Sleep( 20 );
+    Beep( re1, ban );
+    Sleep( ting );
+    Beep( so1, pai );
+    Sleep( ting );
+
+    Beep( mi1, 2 * pai );
+    Sleep( ting + pai );
+    Beep( mi, pai );
+    Sleep( ting );
+
+    Beep( la1, 2 * pai );
+    Sleep( ting );
+    Beep( so1, 2 * pai );
+    Sleep( ting );
+
+    Beep( mi1, ban );
+    Beep( re1, ban );
+    Sleep( ting );
+    Beep( do1, 2 * pai );
+    Sleep( ting + ban );
+    Beep( do1, ban );
+    Sleep( ting );
+
+    Beep( re1, pai );
+    Sleep( ting );
+    Beep( do1, ban );
+    Beep( re1, ban );
+    Sleep( 20 );
+    Beep( re1, ban );
+    Sleep( ting );
+    Beep( si, pai );
+    Sleep( ting );
+
+    Beep( la, 2 * pai );
+    Sleep( ting );
+    Beep( la, ban );
+    Beep( si, ban );
+
+    Beep( do1, pai + ban );
+    Beep( si, ban );
+    Sleep( ting );
+    Beep( do1, pai );
+    Sleep( ting );
+    Beep( mi1, pai );
+    Sleep( ting );
+
+    Beep( si, 3 * pai );
+    Sleep( ting );
+    Beep( mi, ban );
+    Beep( mi, ban );
+
+    Beep( la, ban + pai );
+    Beep( so, ban );
+    Sleep( ting );
+    Beep( la, pai );
+    Sleep( ting );
+    Beep( do1, pai );
+    Sleep( ting );
+
+    Beep( so, 2 * pai );
+    Sleep( ting );
+    Sleep( pai );
+    Beep( mi, ban );
+    Sleep( ting / 2 );
+    Beep( mi, ban );
+    Sleep( ting / 2 );
+
+    Beep( fa, pai + ban );
+    Beep( mi, ban );
+    Sleep( ting );
+    Beep( fa, ban );
+    Beep( do1, ban + pai );
+    Sleep( ting );
+
+    Beep( mi, 2 * pai );
+    Sleep( ting );
+    Sleep( ban );
+    Beep( do1, ban );
+    Sleep( ting / 2 );
+    Beep( do1, ban );
+    Sleep( ting / 2 );
+    Beep( do1, ban );
+    Sleep( ting / 2 );
+
+    Beep( si, ban + pai );
+    Beep( sfa, ban );
+    Sleep( ting );
+    Beep( sfa, pai );
+    Beep( si, pai );
+    Sleep( ting );
+
+    Beep( si, 2 * pai );
+    Sleep( ting );
+    Sleep( pai );
+    Beep( la, ban );
+    Beep( si, ban );
+    Sleep( ting );
+
+    Beep( do1, pai + ban );
+    Beep( si, ban );
+    Sleep( ting );
+    Beep( do1, pai );
+    Sleep( ting );
+    Beep( mi1, pai );
+    Sleep( ting );
+
+    Beep( si, 2 * pai );
+    Sleep( ting );
+    Sleep( pai );
+    Beep( mi, ban );
+    Sleep( 20 );
+    Beep( mi, ban );
+    Sleep( ting );
+
+    Beep( la, pai + ban );
+    Beep( so, ban );
+    Sleep( ting );
+    Beep( la, pai );
+    Sleep( ting );
+    Beep( do1, pai );
+    Sleep( ting );
+
+    Beep( so, 3 * pai );
+    Sleep( ting + ban );
+    Beep( mi, ban );
+    Sleep( ting / 2 );
+
+    Beep( fa, pai );
+    Sleep( ting );
+    Beep( do1, ban );
+    Beep( si, ban );
+    Sleep( 20 );
+    Beep( si, pai );
+    Sleep( ting );
+    Beep( do1, pai );
+    Sleep( ting );
+
+    Beep( re1, ban );
+    Sleep( 20 );
+    Beep( re1, ban );
+    Sleep( 20 );
+    Beep( mi1, ban );
+    Sleep( ting / 2 );
+    Beep( do1, pai );
+    Sleep( ting + pai );
+
+    Beep( la, 4 * pai );
+
+    Sleep( 1000 );
+
+    return(0);
+}
