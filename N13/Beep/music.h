ad368b2f1f0c00cd6dc3bdefeb5330e6f278a0b7
Dynesshely <2369913161@qq.com>
2022-03-10 11:14:30 +0800

    😀 提取基类

---
+++ N13/Beep/music.h
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#include <windows.h>
+
+#define qdo	262
+#define qre	294
+#define qmi	330
+#define qfa	349
+#define qso	392
+#define qla	440
+#define qsi	494
+#define do	523
+#define re	578
+#define mi	659
+#define fa	698
+#define so	784
+#define la	880
+#define si	988
+#define do1	1046
+#define re1	1175
+#define mi1	1318
+#define fa1	1480
+#define so1	1568
+#define la1	1760
+#define si1	1976
+#define sqdo	277
+#define sqre	311
+#define sqfa	370
+#define sqso	415
+#define sqla	466
+#define sdo	554
+#define sre	622
+#define sfa	740
+#define sso	831
+#define sla	932
+#define sdo1	1046
+#define sre1	1245
+#define sfa1	1480
+#define sso1	1661
+#define sla1	1865
