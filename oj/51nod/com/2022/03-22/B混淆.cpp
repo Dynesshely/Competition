4ad8c08198b0722a31c9ce5c244a44e6b6a75660
Dynesshely <2369913161@qq.com>
2022-03-22 13:26:44 +0800

    ✔️ @Ctkon, @weclont

--- oj/51nod/com/2022/03-22/B混淆.cpp
+++ oj/51nod/com/2022/03-22/B混淆.cpp
@@ -1,104 +1,104 @@
 #include <bits/stdc++.h>
-#define ReplacementFor_ll long long
+#define l_ll long long
 #define ReplacementFor_EOF EOF
 #define ReplacementFor_T T
-#define ReplacementFor_a a
-#define ReplacementFor_add add
-#define ReplacementFor_ans ans
-#define ReplacementFor_ask ask
-#define ReplacementFor_buf buf
-#define ReplacementFor_c c
-#define ReplacementFor_cout cout
-#define ReplacementFor_ept ept
-#define ReplacementFor_erase erase
-#define ReplacementFor_fread fread
-#define ReplacementFor_fwrite fwrite
-#define ReplacementFor_gc gc
-#define ReplacementFor_getchar getchar
-#define ReplacementFor_i i
-#define ReplacementFor_insert insert
-#define ReplacementFor_it it
-#define ReplacementFor_lb lb
-#define ReplacementFor_memset memset
-#define ReplacementFor_min min
-#define ReplacementFor_mn mn
-#define ReplacementFor_n n
-#define ReplacementFor_op op
-#define ReplacementFor_pc pc
-#define ReplacementFor_push_back push_back
-#define ReplacementFor_read read
-#define ReplacementFor_s s
-#define ReplacementFor_set set
-#define ReplacementFor_stdin stdin
-#define ReplacementFor_stdout stdout
-#define ReplacementFor_sum sum
-#define ReplacementFor_t t
-#define ReplacementFor_tt tt
-#define ReplacementFor_v v
-#define ReplacementFor_val val
-#define ReplacementFor_vector vector
-#define ReplacementFor_w w
-#define ReplacementFor_wt wt
-#define ReplacementFor_wts wts
-#define ReplacementFor_x x
-#define ReplacementFor_xx xx
-#define ReplacementFor_y y
+#define a_a a
+#define a_add add
+#define a_ans ans
+#define a_ask ask
+#define b_buf buf
+#define c_c c
+#define c_cout cout
+#define e_ept ept
+#define e_erase erase
+#define f_fread fread
+#define f_fwrite fwrite
+#define g_gc gc
+#define g_getchar getchar
+#define i_i i
+#define i_insert insert
+#define i_it it
+#define l_lb lb
+#define m_memset memset
+#define m_min min
+#define m_mn mn
+#define n_n n
+#define o_op op
+#define p_pc pc
+#define p_push_back push_back
+#define r_read read
+#define s_s s
+#define s_set set
+#define s_stdin stdin
+#define s_stdout stdout
+#define s_sum sum
+#define t_t t
+#define t_tt tt
+#define v_v v
+#define v_val val
+#define v_vector vector
+#define w_w w
+#define w_wt wt
+#define w_wts wts
+#define x_x x
+#define x_xx xx
+#define y_y y
 
 using namespace std;
-char ReplacementFor_gc(){
-    static char ReplacementFor_buf[1 << 16], *ReplacementFor_s, *ReplacementFor_t; if ( ReplacementFor_s == ReplacementFor_t ){
-        ReplacementFor_t = (ReplacementFor_s = ReplacementFor_buf) + ReplacementFor_fread( ReplacementFor_buf, 1, 1 << 16, ReplacementFor_stdin );
-        if ( ReplacementFor_s == ReplacementFor_t ) return(ReplacementFor_EOF);
+char g_gc(){
+    static char b_buf[1 << 16], *s_s, *t_t; if ( s_s == t_t ){
+        t_t = (s_s = b_buf) + f_fread( b_buf, 1, 1 << 16, s_stdin );
+        if ( s_s == t_t ) return(ReplacementFor_EOF);
     }
-    return(*ReplacementFor_s++);
+    return(*s_s++);
 }
-int ReplacementFor_read(){
-    char ReplacementFor_c; int	ReplacementFor_w = 1;
-    while ( (ReplacementFor_c = ReplacementFor_getchar() ) > ((char)(0xda5+1702-0x1412)) || ReplacementFor_c < ((char)(0x982+2569-0x135b)) )
-        if ( ReplacementFor_c == ((char)(0x49c+8370-0x2521)) ) ReplacementFor_w = -1;
-    int ReplacementFor_ans = ReplacementFor_c - ((char)(0xbbd+1080-0xfc5));
-    while ( (ReplacementFor_c = ReplacementFor_getchar() ) >= ((char)(0xab+7876-0x1f3f)) && ReplacementFor_c <= ((char)(0x44d+7576-0x21ac)) )
-        ReplacementFor_ans = (ReplacementFor_ans << 1) + (ReplacementFor_ans << 3) + ReplacementFor_c - ((char)(0xfba+800-0x12aa));
-    return(ReplacementFor_ans * ReplacementFor_w);
+int r_read(){
+    char c_c; int	w_w = 1;
+    while ( (c_c = g_getchar() ) > ((char)(0xda5+1702-0x1412)) || c_c < ((char)(0x982+2569-0x135b)) )
+        if ( c_c == ((char)(0x49c+8370-0x2521)) ) w_w = -1;
+    int a_ans = c_c - ((char)(0xbbd+1080-0xfc5));
+    while ( (c_c = g_getchar() ) >= ((char)(0xab+7876-0x1f3f)) && c_c <= ((char)(0x44d+7576-0x21ac)) )
+        a_ans = (a_ans << 1) + (a_ans << 3) + c_c - ((char)(0xfba+800-0x12aa));
+    return(a_ans * w_w);
 }
-void ReplacementFor_pc( char ReplacementFor_c, int ReplacementFor_op ){
-    static char ReplacementFor_buf[1 << 16], *ReplacementFor_s = ReplacementFor_buf, *ReplacementFor_t = ReplacementFor_buf + (1 << 16);
-    (ReplacementFor_op || ( (*ReplacementFor_s++ = ReplacementFor_c) && ReplacementFor_s == ReplacementFor_t) ) && ( (ReplacementFor_fwrite( ReplacementFor_buf, 1, ReplacementFor_s - ReplacementFor_buf, ReplacementFor_stdout ) ), ReplacementFor_s = ReplacementFor_buf);
+void p_pc( char c_c, int o_op ){
+    static char b_buf[1 << 16], *s_s = b_buf, *t_t = b_buf + (1 << 16);
+    (o_op || ( (*s_s++ = c_c) && s_s == t_t) ) && ( (f_fwrite( b_buf, 1, s_s - b_buf, s_stdout ) ), s_s = b_buf);
 }
-void ReplacementFor_wt( int ReplacementFor_x ){ if ( ReplacementFor_x > 9 ) ReplacementFor_wt( ReplacementFor_x / 10 ); ReplacementFor_pc( ((char)(0x88f+7786-0x26c9)) + ReplacementFor_x % 10, 0 ); }
-void ReplacementFor_wts( int ReplacementFor_x, char ReplacementFor_op ){ if ( ReplacementFor_x < 0 ) ReplacementFor_pc( ((char)(0x2000+516-0x21d7)), 0 ), ReplacementFor_x = -ReplacementFor_x; ReplacementFor_wt( ReplacementFor_x ); ReplacementFor_pc( ReplacementFor_op, 0 ); }
-const int ReplacementFor_xx = 2e5 + 7;
-int ReplacementFor_n, ReplacementFor_a[ReplacementFor_xx], ReplacementFor_t[ReplacementFor_xx], ReplacementFor_sum[ReplacementFor_xx], ReplacementFor_tt[ReplacementFor_xx];
-int ReplacementFor_lb( int ReplacementFor_x ){ return(ReplacementFor_x & - ReplacementFor_x); }
-void ReplacementFor_add( int ReplacementFor_x, int ReplacementFor_y ){ for (; ReplacementFor_x <= ReplacementFor_n; ReplacementFor_x += ReplacementFor_lb( ReplacementFor_x ) ) ReplacementFor_sum[ReplacementFor_x] += ReplacementFor_y;}
-int ReplacementFor_ask( int ReplacementFor_x ){ int ReplacementFor_ans = 0; for (; ReplacementFor_x; ReplacementFor_x -= ReplacementFor_lb( ReplacementFor_x ) ) ReplacementFor_ans += ReplacementFor_sum[ReplacementFor_x]; return(ReplacementFor_ans); }
+void w_wt( int x_x ){ if ( x_x > 9 ) w_wt( x_x / 10 ); p_pc( ((char)(0x88f+7786-0x26c9)) + x_x % 10, 0 ); }
+void w_wts( int x_x, char o_op ){ if ( x_x < 0 ) p_pc( ((char)(0x2000+516-0x21d7)), 0 ), x_x = -x_x; w_wt( x_x ); p_pc( o_op, 0 ); }
+const int x_xx = 2e5 + 7;
+int n_n, a_a[x_xx], t_t[x_xx], s_sum[x_xx], t_tt[x_xx];
+int l_lb( int x_x ){ return(x_x & - x_x); }
+void a_add( int x_x, int y_y ){ for (; x_x <= n_n; x_x += l_lb( x_x ) ) s_sum[x_x] += y_y;}
+int a_ask( int x_x ){ int a_ans = 0; for (; x_x; x_x -= l_lb( x_x ) ) a_ans += s_sum[x_x]; return(a_ans); }
 int main(){
-    int ReplacementFor_T = ReplacementFor_read();
+    int ReplacementFor_T = r_read();
     while ( ReplacementFor_T-- ){
-        ReplacementFor_memset( ReplacementFor_sum, 0, sizeof(ReplacementFor_sum) );
-        ReplacementFor_memset( ReplacementFor_t, 0, sizeof(ReplacementFor_t) );
-        ReplacementFor_memset( ReplacementFor_tt, 0, sizeof(ReplacementFor_tt) );
-        ReplacementFor_n = ReplacementFor_read();
-        for ( int ReplacementFor_i = 1; ReplacementFor_i <= ReplacementFor_n; ReplacementFor_i++ ) ReplacementFor_a[ReplacementFor_i] = ReplacementFor_read();
-        int	ReplacementFor_mn	= 1e9, ReplacementFor_ept = 0; ReplacementFor_ll	ReplacementFor_val	= 0;
-        for ( int ReplacementFor_i = 1; ReplacementFor_i <= ReplacementFor_n; ReplacementFor_i++ ) ReplacementFor_t[ReplacementFor_i] = ReplacementFor_sum[ReplacementFor_i] = 0;
-        ReplacementFor_set<int>ReplacementFor_s;
-        for ( int ReplacementFor_i = 1; ReplacementFor_i <= ReplacementFor_n; ReplacementFor_i++ ){
-            ReplacementFor_mn = ReplacementFor_min( ReplacementFor_mn, ReplacementFor_a[ReplacementFor_i] );
-            if ( ReplacementFor_mn == ReplacementFor_a[ReplacementFor_i] ) ReplacementFor_ept++, ReplacementFor_tt[ReplacementFor_a[ReplacementFor_i]]++, ReplacementFor_s.ReplacementFor_insert( ReplacementFor_a[ReplacementFor_i] );
-            ReplacementFor_t[ReplacementFor_a[ReplacementFor_i]]++; if ( ReplacementFor_t[ReplacementFor_a[ReplacementFor_i]] == 1 ) ReplacementFor_add( ReplacementFor_a[ReplacementFor_i], 1 );
-            ReplacementFor_val += ReplacementFor_ask( ReplacementFor_a[ReplacementFor_i] - 1 ); ReplacementFor_vector<int>ReplacementFor_v;
-            for ( auto ReplacementFor_it : ReplacementFor_s ){
-                if ( ReplacementFor_it > ReplacementFor_mn && ReplacementFor_it < ReplacementFor_a[ReplacementFor_i] ){
-                    ReplacementFor_tt[ReplacementFor_it]--; if ( ReplacementFor_tt[ReplacementFor_it] == 0 ) ReplacementFor_v.ReplacementFor_push_back( ReplacementFor_it ); ReplacementFor_ept--;
+        m_memset( s_sum, 0, sizeof(s_sum) );
+        m_memset( t_t, 0, sizeof(t_t) );
+        m_memset( t_tt, 0, sizeof(t_tt) );
+        n_n = r_read();
+        for ( int i_i = 1; i_i <= n_n; i_i++ ) a_a[i_i] = r_read();
+        int	m_mn	= 1e9, e_ept = 0; l_ll	v_val	= 0;
+        for ( int i_i = 1; i_i <= n_n; i_i++ ) t_t[i_i] = s_sum[i_i] = 0;
+        s_set<int>s_s;
+        for ( int i_i = 1; i_i <= n_n; i_i++ ){
+            m_mn = m_min( m_mn, a_a[i_i] );
+            if ( m_mn == a_a[i_i] ) e_ept++, t_tt[a_a[i_i]]++, s_s.i_insert( a_a[i_i] );
+            t_t[a_a[i_i]]++; if ( t_t[a_a[i_i]] == 1 ) a_add( a_a[i_i], 1 );
+            v_val += a_ask( a_a[i_i] - 1 ); v_vector<int>v_v;
+            for ( auto i_it : s_s ){
+                if ( i_it > m_mn && i_it < a_a[i_i] ){
+                    t_tt[i_it]--; if ( t_tt[i_it] == 0 ) v_v.p_push_back( i_it ); e_ept--;
                 }
-                if ( ReplacementFor_it > ReplacementFor_a[ReplacementFor_i] ) break;
+                if ( i_it > a_a[i_i] ) break;
             }
-            for ( auto ReplacementFor_it : ReplacementFor_v ) ReplacementFor_s.ReplacementFor_erase( ReplacementFor_it );
-            ReplacementFor_cout << ReplacementFor_val + ReplacementFor_i - ReplacementFor_ept << " \n"[ReplacementFor_i == ReplacementFor_n];
+            for ( auto i_it : v_v ) s_s.e_erase( i_it );
+            c_cout << v_val + i_i - e_ept << " \n"[i_i == n_n];
         }
     }
-    ReplacementFor_pc( ((char)(0x1c18+2615-0x261e)), 1 );
+    p_pc( ((char)(0x1c18+2615-0x261e)), 1 );
     return(0);
 }
 
---
+++ oj/51nod/com/2022/03-22/C.cpp
@@ -0,0 +1,174 @@
+#include <cstdio>
+#include <cstdlib>
+const int sz = 40400;
+const int ljz = 1000000007;
+int Tm(int x, int y){long long a = x, b = y; return (a * b) % ljz;}
+void MOD(int &x){if (x >= ljz) x -= ljz; if (x < 0) x += ljz;}
+int fpm(int x, int y){int p = 1, s = x; for (; y; y >>= 1){if (y & 1) p = Tm(p,
+s); s = Tm(s, s);} return p;}
+const int R = 1200000;
+int fac[R + 42], ifac[R + 42];
+int K;
+int X[sz], Y[sz], F[sz];
+int x, y, ax, ay, bx, by;
+int C(int n, int k)
+{if (n < k) return 0; return Tm(fac[n], Tm(ifac[k], ifac[n - k]));}
+void prepare(){
+int i;
+for (i = fac[0] = 1; i <= R; i++) fac[i] = Tm(fac[i - 1], i);
+ifac[R] = fpm(fac[R], ljz - 2);
+for (i = R; i; i--) ifac[i - 1] = Tm(ifac[i], i);
+}
+bool turn(int dx, int dy, int &a, int &b){
+int Bz = dy * ax - dx * ay, Bm = by * ax - bx * ay;
+if (Bz % Bm == 0 && (Bz / Bm) >= 0) b = Bz / Bm; else return 0;
+int Az = dx - b * bx, Am = ax;
+if (!ax){Az = dy - b * by; Am = ay;}
+if (Az % Am == 0 && (Az / Am) >= 0) a = Az / Am; else return 0;
+return 1;
+}
+void init(){
+int i, j;
+scanf("%d %d %d", &x, &y, &K);
+scanf("%d %d %d %d", &ax, &ay, &bx, &by);
+for (i = 1; i <= K; i++){
+scanf("%d %d", &X[i], &Y[i]);
+for (j = 1; j < i; j++)
+if (X[i] == X[j] && Y[i] == Y[j]) break;
+if (j < i) K--, i--;
+}
+}
+int dp(int a){
+int i, c, d;
+if (F[a]) return F[a];
+if (turn(X[a], Y[a], c, d)){
+F[a] = C(c + d, c);
+for (i = 1; i <= K; i++)
+if (X[a] != X[i] || Y[a] != Y[i])
+if (turn(X[a] - X[i], Y[a] - Y[i], c, d))
+MOD(F[a] -= Tm(C(c + d, c), dp(i)));
+}
+return F[a];
+}
+void doit1(){
+int i;
+K++; X[K] = x; Y[K] = y;
+for (i = 1; i <= K; i++) F[i] = 0;
+printf("%d\n", dp(K));
+}
+int node[sz], next[sz], to[sz], e;
+int NAL[sz], use[sz];
+int S, T, A, B, shift = 1002;
+void ins(int x, int y){e++; next[e] = node[x]; node[x] = e; to[e] = y;}
+int _node[sz], _next[sz], _to[sz], _;
+void _ins(int x, int y){_++; _next[_] = _node[x]; _node[x] = _; _to[_] = y;}
+int q[sz], inq[sz];
+void bfs(int s){
+int i, j, k, l, r;
+for (i = 1; i < shift * 2; i++) inq[i] = 0, use[i] = 1;
+inq[q[l = r = 0] = s] = 1;
+while (l <= r){
+k = q[l++];
+for (j = node[k]; j; j = next[j])
+if (!inq[to[j]]) inq[q[++r] = to[j]] = 1;
+}
+for (i = 1; i < shift * 2; i++) if (!inq[i]) use[i] = 0;
+}
+void rbfs(int s){
+int i, j, k, l, r;
+for (i = 1; i < shift * 2; i++) inq[i] = 0;
+inq[q[l = r = 0] = s] = 1;
+while (l <= r){
+k = q[l++];
+for (j = _node[k]; j; j = _next[j])
+if (!inq[_to[j]]) inq[q[++r] = _to[j]] = 1;
+}
+for (i = 1; i < shift * 2; i++) if (!inq[i]) use[i] = 0;
+}
+int deg[sz], QL;
+int topsort(){
+int i, j, k, l, r;
+for (i = 1; i < shift * 2; i++) deg[i] = 0;
+for (i = 1; i < shift * 2; i++)
+if (use[i])
+for (j = node[i]; j; j = next[j])
+if (use[to[j]])
+deg[to[j]]++;
+l = 1; r = 0;
+for (i = 1; i < shift * 2; i++)
+if (deg[i] == 0 && use[i])
+q[++r] = i;
+while (l <= r){
+k = q[l++];
+for (j = node[k]; j; j = next[j])
+if (use[to[j]]){
+deg[to[j]]--;
+if (deg[to[j]] == 0)
+q[++r] = to[j];
+}
+}
+for (i = 1; i < shift * 2; i++)
+if (deg[i] > 0 && use[i]) return 0;
+QL = r;
+return 1;
+}
+int f[sz];
+int dp2(int t){
+int i, j, k;
+for (i = 1; i < shift * 2; i++) f[i] = 0;
+f[shift] = 1;
+for (i = 1; i <= QL; i++){
+k = q[i];
+for (j = node[k]; j; j = next[j])
+if (use[to[j]]) MOD(f[to[j]] += f[k]);
+}
+return f[t];
+}
+void doit2(){
+int i;
+if (ax == 0 && ay == 0) {ax = bx; ay = by; bx = by = 0;}
+if (ax == 0 && ay == 0 && bx == 0 && by == 0){
+if (x == 0 && y == 0) printf("-1\n"); else printf("0\n");
+return;
+}
+if (ax * y != ay * x) {printf("0\n"); return;}
+for (i = 1; i <= e; i++) next[i] = to[i] = _next[i] = _to[i] = 0;
+for (i = -1001; i <= 1001; i++) node[shift + i] = _node[shift + i] = 0;
+e = _ = 0;
+for (i = -1001; i <= 1001; i++) NAL[shift + i] = 0;
+if (ax != 0 || bx != 0){
+T = x;
+for (i = 1; i <= K; i++)
+if (ax * Y[i] == ay * X[i]) NAL[X[i] + shift] = 1;
+A = ax; B = bx;
+} else {
+T = y;
+for (i = 1; i <= K; i++)
+if (ax * Y[i] == ay * X[i]) NAL[Y[i] + shift] = 1;
+A = ay; B = by;
+}
+S = shift; T += shift;
+for (i = -1001; i <= 1001; i++)
+if (!NAL[i + shift]){
+if (i + A <= 1001 && i + A >= -1001 && !NAL[i + A + shift])
+ins(i + shift, i + A + shift), _ins(i + A + shift, i + shift);
+if (A != B)
+if (i + B <= 1001 && i + B >= -1001 && !NAL[i + B + shift])
+ins(i + shift, i + B + shift), _ins(i + B + shift, i + shift);
+}
+ins(shift - 1001, shift - 1001); _ins(shift - 1001, shift - 1001);
+ins(shift + 1001, shift + 1001); _ins(shift + 1001, shift + 1001);
+bfs(S); rbfs(T);
+if (topsort()) printf("%d\n", dp2(T)); else printf("-1\n");
+}
+int main(){
+int t;
+prepare();
+scanf("%d", &t);
+while (t--){
+init();
+if (ax * by != ay * bx) doit1();
+else doit2();
+}
+return 0;
+}
