dbc8e36cec955a0e628dbdec20e3500ac44932a4
Dynesshely <2369913161@qq.com>
2022-03-25 11:11:31 +0800

    ✅ 205.

---
+++ oj/51nod/com/2022/03-24/A.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n, k;
+unsigned int s, x, a[(int)3e6 + 5];
+int main(){
+    scanf("%d %d %ud", &n, &k, &x);
+    --k;
+    for(int i = 1; i <= n; ++ i){
+        a[i] = x;
+        x = 1145141 * x + 1919 * (i + 1) + 810;
+    }
+    for(int i = 0; i <= 21; ++ i){
+        if(k & (1 << i)){
+            n -= 1 << i;
+            for(int j = 1; j <= n; ++ j){
+                a[j] ^= a[j + (1 << i)];
+            }
+        }
+    }
+    for(int i = 1; i <= n; ++ i){
+        s += (long long)1 * i * (i ^ a[i]);
+    }
+    printf("%u\n", s);
+    return 0;
+}
---
+++ oj/51nod/com/2022/03-24/C.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int maxn = 2e6 + 5;
+int n, m, x, y, type, z, cnt, h[maxn], opt[maxn], val[maxn];
+int fa[maxn], d[maxn], size[maxn], son[maxn], top[maxn];
+int Time, in[maxn], out[maxn], pos[maxn];
+int s[2][maxn << 1], 
+struct edge{ int to, ne; }e[maxn << 1];
+void add(int x, int y){ e[++ cnt] = (edge){ y, h[x] }; h[x] = cnt; }
+void dfs1(int x){
+    int i, y;
+}
+int main(){
+    
+    return 0;
+}
