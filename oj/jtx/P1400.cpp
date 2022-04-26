14b32ae21763db8a8a37de1c0785b7b6889db407
Dynesshely <catrol@qq.com>
2022-04-26 23:59:46 +0800

    ✔ Pass([JTX]P1400.cpp): ...

--- oj/jtx/P1400.cpp
+++ oj/jtx/P1400.cpp
@@ -1,3 +1,6 @@
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "cert-err58-cpp"
+#pragma ide diagnostic ignored "cert-err34-c"
 
 // Copyright (c) Catrol 2022-present.
 // ALL RIGHTS RESERVED.
@@ -7,123 +10,117 @@
 
 #include <bits/stdc++.h>
 
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-register"
 
+const int maxsize = 200 + 3;
 
-//#include <bits/stdc++.h>
-//using namespace std;
-//
-//const int maxn = 210, maxm = 210;
-//int n, m, T, a[maxn][maxm], angle, total_angle = 0;
-//
-//int angle_converter(int src){
-//    switch(src){
-//        case -270: return 90;
-//        case -180: return 180;
-//        case -90: return 270;
-//        default: return 0;
-//    }
-//}
-//
-//void rotate(){
-//    int for_num = total_angle > 0 ? (total_angle - ((int)(total_angle / 360) * 360)) / 90 :
-//        ((angle_converter(abs(total_angle) - ((int)(abs(total_angle) / 360) * 360))) / 90);
-//
-//    total_angle = 0;
-//
-//}
-//
-//int main(){
-//    scanf("%d %d", &n, &m);
-//    for(int i = 1; i <= n; i++)
-//        for(int j = 1; j <= m; j++)
-//            scanf("%d", &a[i][j]);
-//    scanf("%d", &T);
-//    for(int i = 1; i <= T; ++ i){
-//        char op; cin >> op;
-//        if(op == 'h' || op == 'v'){
-//            rotate();
-//
-//        }else{
-//            scanf("%d", &angle);
-//            total_angle += (op == 'l' ? -angle : angle); // 计算总角度
-//        }
-//    }
-//    rotate();
-//    for(int i = 1; i <= n; i++){
-//        for(int j = 1; j <= m; j++)
-//            printf("%d ", a[i][j]);
-//        printf("\n");
-//    }
-//    return 0;
-//}
-//
-//// #include <bits/stdc++.h>
-//// using namespace std;
-//
-//// int n, m, T, a[205][205]; // n -> 行数, m -> 列数, T -> 操作数量
-//// void rotate(int *total_angle){
-////     if(*total_angle > 0){ // 如果总角度为正，则需要向右旋转
-////         rotate_right:
-////         if(*total_angle >= 360) *total_angle = *total_angle - (int)(*total_angle / 360) * 360;
-////         int for_num = 0;
-////         if(*total_angle == 270) for_num = 3;
-////         else if(*total_angle == 180) for_num = 2;
-////         else if(*total_angle == 90) for_num = 1;
-////         for(int q = 1; q <= for_num; ++ q){
-////             int a_copy[205][205];
-////             memset(a_copy, -1, sizeof(a_copy));
-////             // 旋转矩阵到新数组
-////             for(int i = m, ni = m; i >= 1; -- i, -- ni)
-////                 for(int j = 1, nj = n; j <= n; ++ j, -- nj)
-////                     a_copy[ni][nj] = a[i][j];
-////             // int tmp = m, m = n, n = tmp;
-////             memset(a, -1, sizeof(a));
-////             // 拷贝新数组到原数组
-////             for(int i = 1; i <= max(m, n); ++ i)
-////                 for(int j = 1; j <= max(m, n); ++ j)
-////                     if(a_copy[i][j] != -1) a[i][j] = a_copy[i][j];
-////             // for(int i = 0; i <= max(m, n); ++ i){
-////             //     for(int j = 0; j <= max(m, n); ++ j)
-////             //         printf("%d ", a_copy[i][j]);
-////             //     printf("\n");
-////             // }
-////         }
-////     }else{ // 如果总角度为负，则需要向左旋转
-////         if(abs(*total_angle) % 360 == 0) return;
-////         else if(abs(*total_angle) % 270 == 0) *total_angle = 90;
-////         else if(abs(*total_angle) % 180 == 0) *total_angle = 180;
-////         else *total_angle = 270;
-////         goto rotate_right;
-////     }
-////     *total_angle = 0;
-//// }
-//
-//// int main(){
-////     scanf("%d%d", &n, &m);
-////     for(int i = 1; i <= n; ++ i)
-////         for(int j = 1; j <= m; ++ j)
-////             scanf("%d", &a[i][j]);
-////     scanf("%d", &T);
-////     int total_angle = 0; // 总角度
-////     for(int i = 1; i <= T; ++ i){
-////         char op; cin >> op; // 获取操作符
-////         if(op == 'l' || op == 'r'){
-////             int angle; scanf("%d", &angle); // 获取角度
-////             total_angle += (op == 'l' ? -angle : angle); // 计算总角度
-////         }else{
-////             if(total_angle != 0){ rotate(&total_angle); } // 如果总角度不为0，则需要旋转
-////             if(op == 'h'){ // 如果操作符为h，则需要按照横向翻转
-//
-////             }else{ // 如果操作符为v，则需要按照纵向翻转
-//
-////             }
-////         }
-////     }
-////     rotate(&total_angle); // 最后需要再次检查总角度是否为0
-////     for(int i = 1; i <= max(m, n); ++ i){
-////         for(int j = 1; j <= max(m, n); ++ j)
-////             if(a[i][j] != -1) printf("%d ", a[i][j]);
-////         printf("\n");
-////     }
-////     return 0;
-//// }
+int n, m, a[maxsize][maxsize], T, cur_w, cur_h;
+std::queue<std::pair<char, int>> cmds;
+
+inline void input(), process(), output();
+
+int main() {
+    input();
+    process();
+    output();
+    return 0;
+}
+
+inline void input() {
+    scanf("%d %d", &n, &m);
+    cur_h = n, cur_w = m;
+    for (int i = 0; i < n; ++i)
+        for (int j = 0; j < m; ++j)
+            scanf("%d", &a[i][j]);
+    scanf("%d", &T);
+    char o;
+    for (int i = 1, arg; i <= T; ++i) {
+        std::cin >> o;
+        if (o == 'h' || o == 'v') cmds.push({o, 0});
+        else {
+            scanf("%d", &arg);
+            cmds.push({o, arg});
+        }
+    }
+}
+
+template<typename T>
+inline void swap(T *x, T *y) {
+    T tmp = *x;
+    *x = *y;
+    *y = tmp;
+}
+
+inline void rotate() {
+    swap(&cur_h, &cur_w);
+    int **tmp_mat = new int *[cur_h];
+    for (int i = 0; i < cur_h; ++i)
+        tmp_mat[i] = new int[cur_w];
+
+    int newmat_pos_x = 0, newmat_pos_y = 0;
+    for (int j = 0; j < cur_h; ++j)
+        for (int i = cur_w - 1; i >= 0; --i) {
+            tmp_mat[newmat_pos_y][newmat_pos_x] = a[i][j];
+            newmat_pos_y = newmat_pos_x == cur_w - 1 ? newmat_pos_y + 1 : newmat_pos_y;
+            newmat_pos_x = newmat_pos_x == cur_w - 1 ? 0 : newmat_pos_x + 1;
+        }
+
+    for (int i = 0; i < cur_h; ++i)
+        for (int j = 0; j < cur_w; ++j)
+            a[i][j] = tmp_mat[i][j];
+
+    for (int i = 0; i < cur_h; ++i) {
+        delete[]tmp_mat[i];
+        tmp_mat[i] = nullptr;
+    }
+    delete[]tmp_mat;
+    tmp_mat = nullptr;
+}
+
+inline void rotate(int *angle) {
+    int times = *angle >= 0 ? (*angle % 360) / 90 : (
+            *angle <= -360 ? (360 + -1 * (std::abs(*angle) % 360)) / 90 : (*angle + 360) / 90
+    );
+
+    while (times--)
+        rotate();
+    *angle = 0;
+}
+
+inline void process() {
+    int angle = 0, size = cmds.size();
+    for (int i = 0; i < size; ++i) {
+        std::pair<char, int> cur = cmds.front();
+        if (cur.first == 'r') angle += cur.second;
+        else if (cur.first == 'l') angle -= cur.second;
+        else {
+            rotate(&angle);
+            switch (cur.first) {
+                case 'h':
+                    for (int l = 0; l < cur_h; ++l)
+                        for (int j = 0, je = cur_w - 1; j < cur_w / 2; ++j, --je)
+                            swap(&a[l][j], &a[l][je]);
+                    break;
+                case 'v':
+                    for(int j = 0; j < cur_w; ++ j)
+                        for(int l = 0, le = cur_h - 1; l < cur_h / 2; ++ l, --le)
+                            swap(&a[l][j], &a[le][j]);
+                    break;
+            }
+        }
+        cmds.pop();
+    }
+    rotate(&angle);
+}
+
+inline void output() {
+    for (int i = 0; i < cur_h; ++i) {
+        for (int j = 0; j < cur_w; ++j)
+            printf("%d ", a[i][j]);
+        printf("\n");
+    }
+}
+
+#pragma clang diagnostic pop
+#pragma clang diagnostic pop
