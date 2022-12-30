41622456b5de5ea18d3a24a08ddddc07bd2706ec
Dynesshely <catrol@qq.com>
2022-12-31 07:07:15 +0800

    💾 Feat: Template for simple high precision calculating

---
+++ exp/template/high.precision.cpp
@@ -0,0 +1,58 @@
+#include "bits/stdc++.h"
+
+struct num{
+    int len, *arr;
+
+    num build(int l){
+        num n;
+        n.len = l;
+        int *a = new int[l];
+        for(int i = 0; i < l; i++) a[i] = 0;
+        n.arr = a;
+        return n;
+    }
+
+    num build(int l, int *a){
+        num n;
+        n.len = l, n.arr = a;
+        return n;
+    }
+
+    void print(num *n){
+        num a = *n;
+        for(int i = 0; i < a.len; i++) putchar(a.arr[i] + '0');
+    }
+
+    void println(num *n){
+        (*n).print(n);
+        putchar('\n');
+    }
+
+    num operator +(num a){
+        int bl, ll, al, carry = 0; //  bl -> bigger length, ll -> lower length, al -> actual length, carry -> carry
+        if(len > a.len) bl = len, ll = a.len, al = bl + 1;
+        else bl = a.len, ll = len, al = bl + 1;
+        num rst = build(al);
+        for(int i = 0; i < ll; i++){
+            int sum = arr[i] + a.arr[i] + carry;    //  sum of this turn sum plus last turn carry
+            if (sum >= 10) rst.arr[-(i - al + 1)] = 0, carry = 1;
+            else rst.arr[-(i - al + 1)] = sum, carry = 0;
+        }
+        if(carry > 0) rst.arr[0] = 1;
+        return rst;
+    }
+};
+
+int main(int argc, char* argv[]){
+    int num1[] = {1, 2, 3, 4, 5}, num2[] = {5, 4, 3, 2, 1};
+    num n1 = n1.build(5, num1), n2 = n2.build(5, num2);
+    n1.println(&n1);
+    n2.println(&n2);
+    num rst = n1 + n2;
+    rst.println(&rst);
+    return 0;
+}
+
+
+
+
