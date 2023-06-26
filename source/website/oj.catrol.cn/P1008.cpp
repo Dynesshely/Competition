039ae56dd9e1d352cee947d4f00d75d837a8ceeb
Dynesshely <catrol@qq.com>
2023-06-26 19:02:05 +0800

    ✔️ Feat(oj.catrol.cn): P1008.

---
+++ source/website/oj.catrol.cn/P1008.cpp
@@ -0,0 +1,17 @@
+﻿#include <bits/stdc++.h>
+
+int A = 0, B = 0, s = 10000, e = 99999;
+
+int main() {
+
+    for (int i = s; i <= e; ++ i) {
+        A = i * 10 + 7;
+        B = i + 7 * 100000;
+        if (B == A * 5) {
+            printf("%d\n", i);
+            break;
+        }
+    }
+
+    return 0;
+}
