a8f8ca97bbb916d98dcdc03defb6c963f2ed7410
Dynesshely <catrol@qq.com>
2024-03-14 01:08:19 +0800

    ✅ AC(oj.catrol.cn): `P1005`.

---
+++ source/website/oj.catrol.cn/P1005.cpp
@@ -0,0 +1,29 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+int m, k, *a;
+
+void input() {
+    scanf("%d %d", &m, &k);
+    a = new int[m];
+    for (int i = 0; i < m; i++) {
+        scanf("%d", &a[i]);
+    }
+}
+
+void process() {
+    sort(a, a + m);
+}
+
+void output() {
+    printf("%d\n", a[m - k]);
+    delete[] a;
+}
+
+int main() {
+    input();
+    process();
+    output();
+    return 0;
+}
