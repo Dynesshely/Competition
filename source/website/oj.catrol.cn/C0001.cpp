55652a3a940fd2fe9d4b0f498f531990e637f5b0
Dynesshely <catrol@qq.com>
2024-03-18 01:05:49 +0800

    ✅ AC(JtxOJ): C0001

---
+++ source/website/oj.catrol.cn/C0001.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <string>
+
+constexpr unsigned int XOR_KEY = 0x185;
+
+unsigned int calc_name(const std::string& name, unsigned int xor_key) {
+    unsigned int eax = 0x0;
+    unsigned int edx = 0x0;
+
+    for (char ch : name) {
+        edx &= 0xffffff00;
+        edx += ch;
+        edx ^= xor_key;
+        eax += edx;
+    }
+    return eax;
+}
+
+void keygen() {
+    std::string name;
+    std::cin >> name;
+
+    if (name.length() > 19) {
+        std::cout << "error" << std::endl;
+        return;
+    }
+
+    unsigned int name_calc = calc_name(name, XOR_KEY);
+    std::cout << name_calc << std::endl;
+}
+
+int main() {
+    keygen();
+
+    return 0;
+}
