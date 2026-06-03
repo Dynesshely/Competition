60d4b2b43a63323e979df8e1a5d32350ac06ea0d
Dynesshely <dynesshely@gmail.com>
2026-06-04 02:50:42 +0800

    feat(AC P1427 @ 洛谷): implemented code of P1427 from 洛谷

---
+++ source/website/www.luogu.com.cn/P1427.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a[105], n = 0;
+    while (true) {
+        scanf("%d", &a[n]);
+        if (a[n] == 0) break;
+        ++n;
+    }
+    for (int i = n - 1; i >= 0; --i) {
+        printf("%d%c", a[i], i ? ' ' : '\n');
+    }
+    return 0;
+}
