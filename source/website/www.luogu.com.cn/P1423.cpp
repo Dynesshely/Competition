b88eca8d01db0c46db551ba68e29514ec439ce0c
Dynesshely <dynesshely@gmail.com>
2026-06-04 02:50:41 +0800

    feat(AC P1423 @ 洛谷): implemented code of P1423 from 洛谷

---
+++ source/website/www.luogu.com.cn/P1423.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+double s;
+
+void input() {
+    scanf("%lf", &s);
+}
+
+void process() {
+    int steps = 0;
+    double cur = 2.0, dist = 0.0;
+    while (dist < s) {
+        dist += cur;
+        cur *= 0.98;
+        ++steps;
+    }
+    printf("%d\n", steps);
+}
+
+int main() {
+    input();
+    process();
+    return 0;
+}
