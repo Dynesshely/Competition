81d15c164fe3d97d059a368d87ab6e9ee335ead6
Dynesshely <dynesshely@gmail.com>
2026-06-04 02:50:41 +0800

    feat(AC P1421 @ 洛谷): implemented code of P1421 from 洛谷

---
+++ source/website/www.luogu.com.cn/P1421.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int a, b;
+
+void input() {
+    scanf("%d%d", &a, &b);
+}
+
+void process() {
+    int total = a * 10 + b;
+    printf("%d\n", total / 19);
+}
+
+int main() {
+    input();
+    process();
+    return 0;
+}
