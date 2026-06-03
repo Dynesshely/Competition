2fdbab22e7359c7e26872d0af97a41a2286038a8
Dynesshely <dynesshely@gmail.com>
2026-06-04 02:50:42 +0800

    feat(AC P1426 @ 洛谷): implemented code of P1426 from 洛谷

---
+++ source/website/www.luogu.com.cn/P1426.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    double s, x;
+    scanf("%lf%lf", &s, &x);
+    double l = s - x, r = s + x;
+    double p = 0, d = 7;
+    if (p >= l) {
+        p += d;
+        printf(p > r ? "n\n" : "y\n");
+        return 0;
+    }
+    while (p < l) {
+        p += d;
+        if (p < l) d *= 0.98;
+    }
+    if (p > r) {
+        printf("n\n");
+    } else {
+        d *= 0.98;
+        p += d;
+        printf(p > r ? "n\n" : "y\n");
+    }
+    return 0;
+}
