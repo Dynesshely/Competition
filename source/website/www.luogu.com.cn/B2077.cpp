bbb7c9db4963150ee3ff9c195d16580c2512c7c7
Dynesshely <dynesshely@gmail.com>
2026-06-06 04:38:04 +0800

    feat(AC B2077 @ 洛谷): implemented code of B2077 from 洛谷

--- source/website/www.luogu.com.cn/B2077.cpp
+++ source/website/www.luogu.com.cn/B2077.cpp
@@ -4,8 +4,12 @@ using namespace std;
 typedef long long ll;
 
 void writeInt(ll x) {
-    if (x < 0) { putchar('-'); x = -x; }
-    if (x >= 10) writeInt(x / 10);
+    if (x < 0) {
+        putchar('-');
+        x = -x;
+    }
+    if (x >= 10)
+        writeInt(x / 10);
     putchar('0' + x % 10);
 }
 
@@ -15,8 +19,13 @@ int main() {
     scanf("%lld", &n);
     while (n != 1) {
         writeInt(n);
-        if (n & 1) { n = n * 3 + 1; printf("*3+1="); }
-        else       { n >>= 1;        printf("/2="); }
+        if (n & 1) {
+            n = n * 3 + 1;
+            printf("*3+1=");
+        } else {
+            n >>= 1;
+            printf("/2=");
+        }
         writeInt(n);
         putchar('\n');
     }
