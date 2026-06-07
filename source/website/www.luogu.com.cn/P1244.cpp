34f412400b38de4a377692df9342a3d43d9f506d
Dynesshely <dynesshely@gmail.com>
2026-06-08 04:04:26 +0800

    feat(AC P1244 @ 洛谷): implemented code of P1244 from 洛谷

---
+++ source/website/www.luogu.com.cn/P1244.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+
+void input() { scanf("%d%d", &n, &m); }
+
+void process() {}
+
+void output() { printf("%lld\n", (1LL << n) * (m + 1)); }
+
+int main() {
+    input();
+    process();
+    output();
+    return 0;
+}
