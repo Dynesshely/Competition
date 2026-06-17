7639472823d0a2e4cf11787b05281ce0ceef7e12
Dynesshely <dynesshely@gmail.com>
2026-06-18 06:14:55 +0800

    feat(AC P1425 @ 洛谷): implemented code of P1425 from 洛谷

--- source/website/www.luogu.com.cn/P1425.cpp
+++ source/website/www.luogu.com.cn/P1425.cpp
@@ -5,10 +5,10 @@ int main() {
     int a, b, c, d;
     scanf("%d%d%d%d", &a, &b, &c, &d);
     int start = a * 60 + b;
-    int end = c * 60 + d;
-    int diff = end - start;
-    int h = diff / 60;
-    int m = diff % 60;
+    int end   = c * 60 + d;
+    int diff  = end - start;
+    int h     = diff / 60;
+    int m     = diff % 60;
     printf("%d %d\n", h, m);
     return 0;
 }
