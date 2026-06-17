b23cda055f452bcb2ac2a712ac9df0803c2cde49
Dynesshely <dynesshely@gmail.com>
2026-06-18 06:14:54 +0800

    feat(AC P1424 @ 洛谷): implemented code of P1424 from 洛谷

--- source/website/www.luogu.com.cn/P1424.cpp
+++ source/website/www.luogu.com.cn/P1424.cpp
@@ -3,15 +3,14 @@ using namespace std;
 
 int x, n;
 
-void input() {
-    scanf("%d%d", &x, &n);
-}
+void input() { scanf("%d%d", &x, &n); }
 
 void process() {
     int ans = 0;
     for (int i = 0; i < n; i++) {
         int cur = (x + i - 1) % 7 + 1;
-        if (cur <= 5) ans += 250;
+        if (cur <= 5)
+            ans += 250;
     }
     printf("%d\n", ans);
 }
