df35737cc86d941ceebbd980489559eb672c7b52
Dynesshely <dynesshely@gmail.com>
2026-06-18 06:14:53 +0800

    feat(AC P1420 @ 洛谷): implemented code of P1420 from 洛谷

--- source/website/www.luogu.com.cn/P1420.cpp
+++ source/website/www.luogu.com.cn/P1420.cpp
@@ -5,15 +5,19 @@ int n, a[10010];
 
 void input() {
     scanf("%d", &n);
-    for (int i = 1; i <= n; i++) scanf("%d", &a[i]);
+    for (int i = 1; i <= n; i++)
+        scanf("%d", &a[i]);
 }
 
 int process() {
     int ans = 0, cur = 0;
     for (int i = 1; i <= n; i++) {
-        if (i == 1 || a[i] == a[i - 1] + 1) cur++;
-        else cur = 1;
-        if (cur > ans) ans = cur;
+        if (i == 1 || a[i] == a[i - 1] + 1)
+            cur++;
+        else
+            cur = 1;
+        if (cur > ans)
+            ans = cur;
     }
     return ans;
 }
